36155595cdcad05dfb1e02a70efc4a6facafb0ca
Onnesty <luizterrens@gmail.com>
2026-07-01 21:49:16 +0800

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        int Left = 0;
+        int Right = s.size() - 1;
+
+        while (Left < Right) {
+
+            cout << "L: " << s[Left] << " | R: " << s[Right] << endl; 
+
+            if ( !isalnum(s[Left]) ) {
+                Left++;
+                continue;
+            }
+
+            if ( !isalnum(s[Right]) ) {
+                Right--;
+                continue;
+            }
+
+            if (toupper(s[Left]) != toupper(s[Right])) return false;
+
+            Left++;
+            Right--;
+        }
+
+        return true;
+
+
+
+        
+    }
+};
