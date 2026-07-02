18ac7780905e2dfa1a8c9a7302d046bc6a069ea7
Onnesty <luizterrens@gmail.com>
2026-07-02 18:19:32 +0800

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.cpp
@@ -0,0 +1,36 @@
+class MinStack {
+public:
+
+    vector<int> Stack;
+
+    map<int,int> IntMap;
+
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        Stack.push_back(val);
+
+        IntMap[val]++;
+    }
+    
+    void pop() {
+
+        int val = Stack.back();
+        Stack.pop_back();
+
+        IntMap[val]--;
+
+        if (IntMap[val] == 0) IntMap.erase(val);
+    }
+    
+    int top() {
+        return Stack.back();
+    }
+    
+    int getMin() {
+        return IntMap.begin()->first;
+    }
+};
